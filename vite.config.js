6f0d2e18af9370802374254552fb3763fc97abea
Mo3taseem <mo3data@gmail.com>
2024-10-30 12:50:10 +0300

    debugging 04

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/',
   server: {
     host: '0.0.0.0',  // This binds the server to all network interfaces
     port: 5173         // You can specify the port here (or change it if needed)
