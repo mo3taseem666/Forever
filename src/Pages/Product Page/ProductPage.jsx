2ee3f19290c72b496319e66a8dcc6eb446f856c8
Mo3taseem <mo3data@gmail.com>
2024-10-30 12:36:20 +0300

    debugging02

--- src/Pages/Product Page/ProductPage.jsx
+++ src/Pages/Product Page/ProductPage.jsx
@@ -1,26 +1,41 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import MainPart from './components/MainPart';
 import { useLocation } from 'react-router-dom';
 import Description from './mini components/Description';
 import Title from '../../Golbal Components/Title';
 import RelatedProducts from './components/RelatedProducts';
 import useGetRelated from '../../Golbal Components/GetRelatedProducts';
+import { products } from '../../assets/frontend_assets/assets';
 
 export default function ProductPage() {
    const location = useLocation();
    const product = location.state;
    const relatedProducts = useGetRelated(product);
-
-   console.log(product);
-   
+   const [alt, setAlt] = useState(null);
 
    useEffect(() => {
-      window.scrollTo({top:0,left:0,behavior:'smooth'});
+      if (!product) {
+         setAlt(
+            products.filter(
+               el =>
+                  el._id ==
+                  location?.pathname?.split('/')[
+                     location?.pathname?.split('/')?.length
+                  ]
+            )
+         );
+      }
+   }, [product, location.pathname]);
+
+   console.log(product);
+
+   useEffect(() => {
+      window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
    }, [product]);
 
    return (
       <div className="flex mt-10 flex-col">
-         <MainPart product={product} />
+         <MainPart product={alt} />
          <Description />
          <Title first="RELATED" second="PRODUCTS" />
          <RelatedProducts arr={relatedProducts} />
