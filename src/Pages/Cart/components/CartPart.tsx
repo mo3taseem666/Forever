6ebddd6ac194ac57955a1e07f43057667d8785b2
Mo3taseem <mo3data@gmail.com>
2024-11-03 17:47:06 +0200

    added notice

--- src/Pages/Cart/components/CartPart.tsx
+++ src/Pages/Cart/components/CartPart.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 export default function CartPart() {
-    const [cartItems,setCartItems] = useState([])
-  return (
-    <div>
-        
-    </div>
-  )
+   const [cartItems, setCartItems] = useState([]);
+   return (
+      <div>
+         <p className="text-3xl mt-10 w-full text-center font-medium">
+            UNDER DEVELOPMENT
+         </p>
+      </div>
+   );
 }
